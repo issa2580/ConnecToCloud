52771cc01c887df5285cb841378ea57b72e07314
issa2580 <issadiop2580@gmail.com>
2024-11-19 14:14:46 +0000

    improve app

--- pages/index.tsx
+++ pages/index.tsx
@@ -135,7 +135,7 @@ export default function Home() {
   ];
 
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen overflow-x-hidden">
       {/* Hero Section (inchangé) */}
       <div className="relative min-h-screen bg-blue-600">
         {/* Animated grid background */}
